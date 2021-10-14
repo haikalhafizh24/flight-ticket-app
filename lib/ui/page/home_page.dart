d5703a5866cde0761ad216c7e81cdef78ea453d2
haikalhafizh24 <haikal24muzakirillah@gmail.com>
2021-10-14 19:57:37 +0700

    added destination tile

--- lib/ui/page/home_page.dart
+++ lib/ui/page/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
-import 'package:hai_air/ui/widgets/destination.dart';
+import 'package:hai_air/ui/widgets/destination_card.dart';
+import 'package:hai_air/ui/widgets/destination_tile.dart';
 import '../../shared/theme.dart';
 
 class HomePage extends StatelessWidget {
@@ -88,23 +89,70 @@ class HomePage extends StatelessWidget {
                 city: 'Japan',
                 rating: 4.8,
               ),
-            DestinationCard(
-              imageUrl: 'assets/image_destination5.png',
-              name: 'Unique Tree',
-              city: 'Israel',
-              rating: 4.8,
-            ),
+              DestinationCard(
+                imageUrl: 'assets/image_destination5.png',
+                name: 'Unique Tree',
+                city: 'Israel',
+                rating: 4.8,
+              ),
             ],
           ),
         ),
       );
     }
 
+    Widget newDestination() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: 30,
+          left: defaultMargin,
+          right: defaultMargin,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'New This Year',
+              style:
+                  blackTextStyle.copyWith(fontSize: 18, fontWeight: semiBold),
+            ),
+            const DestinationTile(
+              imageUrl: 'assets/image_destination6.png',
+              name: 'Danau Beratan',
+              location: 'Singajara',
+              rating: 4.6,
+            ),
+            const DestinationTile(
+              imageUrl: 'assets/image_destination7.png',
+              name: 'Sidney Opera',
+              location: 'Australia',
+              rating: 4.8,
+            ),
+            const DestinationTile(
+              imageUrl: 'assets/image_destination8.png',
+              name: 'Roma',
+              location: 'Italy',
+              rating: 4.9,
+            ),
+            const DestinationTile(
+              imageUrl: 'assets/image_destination9.png',
+              name: 'Unique Tree',
+              location: 'Israel',
+              rating: 4.7,
+            ),
+            const DestinationTile(
+              imageUrl: 'assets/image_destination10.png',
+              name: 'Hill Hey',
+              location: 'Monaco',
+              rating: 4.7,
+            )
+          ],
+        ),
+      );
+    }
+
     return ListView(
-      children: [
-        header(),
-        popularDestination(),
-      ],
+      children: [header(), popularDestination(), newDestination()],
     );
   }
 }
---
+++ lib/ui/widgets/destination_tile.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:hai_air/shared/theme.dart';
+
+class DestinationTile extends StatelessWidget {
+  final String imageUrl;
+  final String name;
+  final String location;
+  final double rating;
+
+  const DestinationTile({
+    Key? key,
+    required this.imageUrl,
+    required this.name,
+    required this.location,
+    this.rating = 0.0,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 16),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(18), color: kWhiteColor),
+      child: Row(
+        children: [
+          Container(
+            width: 70,
+            height: 70,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(18),
+              image: DecorationImage(
+                image: AssetImage(imageUrl),
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 16,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style:
+                      blackTextStyle.copyWith(fontSize: 18, fontWeight: medium),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  location,
+                  style:
+                      greyTextStyle.copyWith(fontSize: 14, fontWeight: medium),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(right: 2),
+                width: 20,
+                height: 20,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/Icon_Star.png'))),
+              ),
+              Text(
+                rating.toString(),
+                style: blackTextStyle.copyWith(fontWeight: medium),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
